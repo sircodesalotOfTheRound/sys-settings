857449c81953d8791dd6c464df5a592593b61f0c
Reuben Kuhnert <--global>
2015-03-31 13:08:11 -0500

    Add environment folders

--- hadoop-setup.sh
+++ hadoop-setup.sh
@@ -1,7 +1,11 @@
+
+HADOOP_VERSION=2.6.0
+AS_USER=sircodesalot
+
 function git_hadoop {
   git clone https://github.com/apache/hadoop
   cd hadoop
-  git checkout branch-2.6.0
+  git checkout branch-${HADOOP_VERSION}
   mvn clean install -Pdist -DskipTests -Dmaven.javadoc.skip=true
   cd ..
 }
@@ -28,18 +32,24 @@ function git_hive {
 function remove_jline {
   # Jline causes problems with Hive CLI.
   # http://stackoverflow.com/questions/28997441/hive-startup-error-terminal-initialization-failed-falling-back-to-unsupporte
-  rm hadoop/share/hadoop/yarn/lib/jline-0.9.94.jar
+  rm hadoop/hadoop-dist/target/hadoop-${HADOOP_VERSION}/share/hadoop/yarn/lib/jline-0.9.94.jar
 }
 
 function set_envrionment {
   export PATH=$PATH:/home/sircodesalot/Dev/Cloudera/hadoop/hadoop-dist/target/hadoop-2.6.0/bin:/home/sircodesalot/Dev/Cloudera/hbase/bin
-  export HADOOP_HOME=/home/sircodesalot//Dev/Cloudera/hadoop/hadoop-dist/target/hadoop-2.6.0
+  export HADOOP_HOME=/home/sircodesalot/Dev/Cloudera/hadoop/hadoop-dist/target/hadoop-2.6.0
   export HIVE_HOME=/home/sircodesalot/Dev/Cloudera/hive/packaging/target/apache-hive-1.2.0-SNAPSHOT-bin/apache-hive-1.2.0-SNAPSHOT-bin
+}
+
+function create_warehouse_folder {
+  sudo makedir -p /user/hive/warehouse
+  chown -R ${AS_USER} /user/hive
 }
 
 function post_install {
   remove_jline
   set_environment
+  create_warehouse_folder
 }
 
 function hadoop_all {
